d4672016500e2d1490df689218d8f72c7869a7e6
elimorolli <elimorolli@gmail.com>
2015-11-05 10:32:15 +0100

    esercizioEsame

--- app.js
+++ app.js
@@ -9,15 +9,39 @@
  *  }
  */ 
 
-var tmpl = ' <li id="ID">' +
-           '  <h3>WORD</h3>' +
-           '  <h3 class="solution">SOLUTION</h3>'+
-           ' </li> ';
+var tmpl = "<li id=ID><h3>WORD</h3><h3 class='solution'>SOLUTION</h3></li> ";
 
+var i=0;var inc=0;var cor=0;
 $(document).ready(function(){
-
-
+    
+    for(var i=0;i<data[i].lenght;i++){
+   $(".cards").append(tmpl.replace('WORD',data[i].word_en).replace("ID",i).replace("SOLUTION",data[i].word_de));
+    $(".solution").hide();
+    $(".options").hide();
+    $(".final").hide();
+    $(".cards").click(function(event){
+        $(".solution").show();
+         $(".options").show();
+    });
+    $("btn opt-incorrect").click(function(event){
+        inc++;alert(inc);
+    });
+    $("btn opt-correct").click(function(event){
+        corr++;alert(corr);
+    });
+        if(i==data.lenght){
+        $(".final").show();
+            $("#tot-good").text(corr);
+            var tot=inc+corr;
+            $("#tot").text(tot);
+        }
+}
+    
 });
+var ese={
+    
+}
+
 
 
 
